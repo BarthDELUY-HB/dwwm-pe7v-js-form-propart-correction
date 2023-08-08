16646cd55a0b2d7752e5aa59e682b0582f57e24f
Barth DELUY <barth@barthdeluy.me>
2023-08-08 10:25:59 +0200

    Correction code postal + CGV

--- _js/particulier.js
+++ _js/particulier.js
@@ -2,19 +2,47 @@
 
 function checkIfAtLeastOnePhoneNumber(event)
 {
-    // On bloque l'envoi du formulaire tant qu'on n'a pas vérifié
-    event.preventDefault();
-
     let landlineNumber = document.querySelector('#landLineNumber').value;
     let gsmNumber = document.querySelector('#gsmNumber').value;
 
     if (landlineNumber.length > 0 || gsmNumber.length > 0) {
         return true;
     }
+    document.querySelector('#errorMessage').textContent = "Merci de renseigner au moins un numéro de téléphone !";
+    // On bloque l'envoi du formulaire
+    event.preventDefault();
+    event.stopImmediatePropagation();
+    return false;
+}
+
+function checkZipCode(event)
+{
+    const zipCode = document.querySelector('#privatePostalCode').value;
+    if ( zipCode.length == 5
+        && isNumeric(zipCode)) {
+        return true;
+    }
+    document.querySelector('#errorMessage').textContent = "Le code postal est invalide !";
+    event.preventDefault();
+    event.stopImmediatePropagation();
+    return false;
+}
+
+function checkIfCGVChecked(event)
+{
+    if (document.querySelectorAll('#privateAcceptCGV:checked').length == 1) {
+        return true;
+    }
+    document.querySelector('#privateAcceptCGV').classList.add('CGVUnchecked');
+    document.querySelector('#errorMessage').textContent = "Merci de valider les CGV !";
+    event.preventDefault();
+    event.stopImmediatePropagation();
     return false;
 }
 
 function attachPrivateAccountListeners()
 {
     document.querySelector('#individualForm').addEventListener('submit', checkIfAtLeastOnePhoneNumber);
+    document.querySelector('#individualForm').addEventListener('submit', checkZipCode);
+    document.querySelector('#individualForm').addEventListener('submit', checkIfCGVChecked);
 }
